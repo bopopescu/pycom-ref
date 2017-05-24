22b6234f489d2b45ae7e96a851d2e88ac5c08c9e
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-05-25 01:49:06 +0200

    Explicit socket module imports

--- ref/pycom_esp32/usocket.py
+++ ref/pycom_esp32/usocket.py
@@ -7,7 +7,7 @@ Socket address format(s)
     Functions below which expect a network address, accept it in the format of (ipv4_address, port), where ipv4_address is a string with dot-notation numeric IPv4 address, e.g. "8.8.8.8", and port is integer port number in the range 1-65535. Note the domain names are not accepted as ipv4_address, they should be resolved first using socket.getaddrinfo().
 """
 
-from socket import *
+from socket import socket, getaddrinfo, error, timeout, AF_INET, SOCK_STREAM, SOCK_DGRAM, SOCK_RAW, IPPROTO_UDP, IPPROTO_TCP, SOL_SOCKET, SO_REUSEADDR
 
 AF_LORA=160
 
